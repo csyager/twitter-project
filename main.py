e38bc2ce650e5e87ab2db153801bf1108669b192
Connor <csy7ay@virginia.edu>
2020-02-27 15:02:39 -0500

    set up environment vars and connected to twitter api

---
+++ main.py
@@ -0,0 +1,16 @@
+import twitter
+import os
+from dotenv import load_dotenv
+load_dotenv()
+
+# get key environment variables
+CONSUMER_KEY = os.getenv("CONSUMER_KEY")
+CONSUMER_SECRET = os.getenv("CONSUMER_SECRET")
+ACCESS_TOKEN_KEY = os.getenv("ACCESS_TOKEN_KEY")
+ACCESS_TOKEN_SECRET = os.getenv("ACCESS_TOKEN_SECRET")
+
+api = twitter.Api(consumer_key=CONSUMER_KEY,
+                    consumer_secret=CONSUMER_SECRET,
+                    access_token_key=ACCESS_TOKEN_KEY,
+                    access_token_secret=ACCESS_TOKEN_SECRET)
+
